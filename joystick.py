3fc30b90da4bc0fa0fa76f6fbbe2215b08116af8
Nathan Kau <nathankau@gmail.com>
2020-02-12 03:29:45 +0000

    update messages and services

--- joystick.py
+++ joystick.py
@@ -35,7 +35,7 @@ print("Number of axis:")
 print(JoyAx)
 
 ## Configurable ##
-MESSAGE_RATE = 50
+MESSAGE_RATE = 20
 while True:
     print("running")
 
@@ -46,19 +46,35 @@ while True:
     twist_right = (pygame.joystick.Joystick(0).get_axis(2))
     twist_left = (pygame.joystick.Joystick(0).get_axis(0))
 
+    L2 = pygame.joystick.Joystick(0).get_axis(3)
+    R2 = pygame.joystick.Joystick(0).get_axis(4)
+
     on_right = (pygame.joystick.Joystick(0).get_button(5))
     on_left = (pygame.joystick.Joystick(0).get_button(4))
     l_trigger = (pygame.joystick.Joystick(0).get_axis(3))
+    r_trigger = (pygame.joystick.Joystick(0).get_axis(4))
+
+    square = pygame.joystick.Joystick(0).get_button(0)
+    x = pygame.joystick.Joystick(0).get_button(1)
+    circle = pygame.joystick.Joystick(0).get_button(2)
+    triangle = pygame.joystick.Joystick(0).get_button(3)
 
     (d_pad_x, d_pad_y) = (pygame.joystick.Joystick(0).get_hat(0))
 
-    msg = { "y" : forward_left,
-            "x" : twist_left,
-            "twist" : twist_right,
-            "pitch" : forward_right,
-            "gait_toggle" : on_right,
-            "stance_movement" : d_pad_y,
-            "roll_movement" : d_pad_x,
+    msg = { "ly" : forward_left,
+            "lx" : twist_left,
+            "rx" : twist_right,
+            "ry" : forward_right,
+            "L2" : L2,
+            "R2" : R2,
+            "R1" : on_right,
+            "L1" : on_left,
+            "dpady" : d_pad_y,
+            "dpadx" : d_pad_x,
+            "x": x,
+            "square": square,
+            "circle": circle,
+            "triangle": triangle,
             "message_rate" : MESSAGE_RATE
          }
     drive_pub.send(msg)
